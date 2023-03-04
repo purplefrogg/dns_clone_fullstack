b77ae57320b676858c46d279d2b34c6635f90fd5
purplefrogg23 <purplefroggy23@gmail.com>
2023-03-04 15:01:09 +0500

    refactoring

--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -1,12 +1,9 @@
-import { NextPage } from 'next'
-import Image from 'next/image'
-import { ReactElement } from 'react'
-import { BreadCrumbs } from '~/components/breadCrumbs'
+import { type ReactElement } from 'react'
 import { Layout } from '~/components/layout'
 import { LayoutCatalog } from '~/components/layoutCatalog'
-import { api } from '~/utils/api'
-import { NextPageWithLayout } from '../_app'
-import { CategoryItem } from './components/categoryItem'
+import { api, RouterOutputs } from '~/utils/api'
+import { type NextPageWithLayout } from '../_app'
+import { CategoryItem } from '../../components/categoryItem'
 
 const Page: NextPageWithLayout = () => {
   const { data } = api.category.getAll.useQuery()
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -14,7 +14,7 @@ import { type AppRouter } from '~/server/api/root'
 const getBaseUrl = () => {
   if (typeof window !== 'undefined') return '' // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
+  return `http://127.0.0.1:${process.env.PORT ?? 3000}`
 }
 
 /** A set of type-safe react-query hooks for your tRPC API. */
