ec08aa04af68ff9cc55fb3fb0c7185f456da46f9
purplefrogg23 <purplefroggy23@gmail.com>
2023-04-13 20:56:24 +0500

    feat: add change password

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { type GetServerSideProps, type NextPage } from 'next'
 import { getServerSession } from 'next-auth'
-import { signOut, useSession } from 'next-auth/react'
+import { signOut } from 'next-auth/react'
+import { type FC } from 'react'
+import { type SubmitHandler, useForm } from 'react-hook-form'
 import { authOptions } from '~/server/api/auth'
+import { api, type RouterInputs } from '~/utils/api'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getServerSession(context.req, context.res, authOptions)
@@ -20,16 +24,18 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 }
 
 const Profile: NextPage = () => {
-  const { data } = useSession()
+  const { data } = api.auth.getProfile.useQuery()
   const signOutHandler = () => {
     void signOut()
   }
+  if (!data) return null
   return (
     <div>
       <h1>Profile</h1>
-      <div>name: {data?.user.name}</div>
-      <div>email: {data?.user.email}</div>
-      <div>role: {data?.user.role}</div>
+      <div>name: {data?.name}</div>
+      <div>email: {data?.email}</div>
+      <div>role: {data?.role}</div>
+      <PasswordControl hasPassword={data?.password} />
       <button
         onClick={signOutHandler}
         className='rounded border border-red-600 p-2 text-red-400'
@@ -40,3 +46,44 @@ const Profile: NextPage = () => {
   )
 }
 export default Profile
+
+const PasswordControl: FC<{ hasPassword: boolean }> = ({ hasPassword }) => {
+  const utils = api.useContext()
+  const { mutate, isSuccess } = api.auth.setPassword.useMutation({
+    onSuccess: async () => {
+      await utils.auth.getProfile.refetch()
+      reset()
+    },
+  })
+  const { register, handleSubmit, reset } =
+    useForm<RouterInputs['auth']['setPassword']>()
+  const onSubmit: SubmitHandler<RouterInputs['auth']['setPassword']> = (
+    data
+  ) => {
+    mutate(data)
+  }
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-2'>
+      {hasPassword && (
+        <label className='flex flex-col gap-1'>
+          old password
+          <input
+            {...register('oldPassword', { required: true })}
+            type='password'
+            className='w-44'
+          />
+        </label>
+      )}
+      <label className='flex flex-col gap-1'>
+        new password
+        <input
+          {...register('newPassword', { required: true })}
+          type='password'
+          className='w-44'
+        />
+      </label>
+      {isSuccess && <span className='text-green-500'>password changed</span>}
+      <button className='w-44 border p-2'>change password</button>
+    </form>
+  )
+}
--- src/server/api/routers/auth/auth.controller.ts
+++ src/server/api/routers/auth/auth.controller.ts
@@ -1,3 +1,6 @@
+import { TRPCError } from '@trpc/server'
+import { compare, hash } from 'bcrypt'
+import { z } from 'zod'
 import { authedProcedure, createTRPCRouter } from '../../trpc'
 
 import { publicProcedure } from '../../trpc'
@@ -8,7 +11,50 @@ export const authRouter = createTRPCRouter({
   signUp: publicProcedure.input(signUpSchema).mutation(async ({ input }) => {
     return await authService.signUp(input)
   }),
-  getProfile: authedProcedure.query(({ ctx }) => {
-    return ctx.session.user
+  getProfile: authedProcedure.query(async ({ ctx }) => {
+    const user = await ctx.prisma.user.findFirstOrThrow({
+      where: {
+        id: ctx.session.user.id,
+      },
+      include: {
+        Account: true,
+      },
+    })
+    return {
+      ...user,
+      password: Boolean(user.password),
+    }
   }),
+  setPassword: authedProcedure
+    .input(
+      z.object({ newPassword: z.string(), oldPassword: z.string().optional() })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const user = await ctx.prisma.user.findUniqueOrThrow({
+        where: {
+          id: ctx.session.user.id,
+        },
+      })
+      if (user.password) {
+        const validPassword = await compare(
+          input.oldPassword || '',
+          user.password
+        )
+        if (!validPassword) {
+          throw new TRPCError({
+            code: 'CONFLICT',
+          })
+        }
+      }
+
+      const newPassword = await hash(input.newPassword, 5)
+      return ctx.prisma.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          password: newPassword,
+        },
+      })
+    }),
 })
